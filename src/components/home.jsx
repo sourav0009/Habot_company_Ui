c01aefd68bee1b34a59e0b35d6c90d14da31658b
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-10-28 17:25:34 +0530

    sv32

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -136,15 +136,15 @@ const Home = () => {
             </section>
 
 
-            <section className=' h-[100%]  mr-32 ml-32 mb-10'>
-                <div className="up flex justify-center flex-col items-center space-y-3">
-                    <h1 className=' text-2xl font-bold'>How it works?</h1>
-                    <p className=' text-center font-sans'>Buyers post their needs and review top suppliers, while suppliers complete profiles, connect with <br /> potential buyers, and build successful business relationships, sharing valuable feedback.
+            <section className=' h-[100%]  mr-32 ml-32 mb-20'>
+                <div className="up flex justify-center flex-col items-center space-y-4">
+                    <h1 className=' text-4xl font-bold'>How it works?</h1>
+                    <p className=' text-center font-sans text-xl'>Buyers post their needs and review top suppliers, while suppliers complete profiles, connect with <br /> potential buyers, and build successful business relationships, sharing valuable feedback.
                     </p>
                 </div>
-                <div className=" flex justify-center flex-col items-center mt-5">
+                <div className=" flex justify-center flex-col items-center mt-8">
                     <div className=" flex">
-                        <div className=' bg-cyan-50 h-48 w-72 text-center flex flex-col items-center p-8'>
+                        <div className=' bg-cyan-50 h-60 w-96 text-center flex flex-col items-center p-12 space-y-5'>
                             <svg width="64" height="64" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M35.4476 38.0417C45.9525 38.0417 54.4684 29.5258 54.4684 19.0208C54.4684 8.51592 45.9525 0 35.4476 0C24.9427 0 16.4268 8.51592 16.4268 19.0208C16.4268 29.5258 24.9427 38.0417 35.4476 38.0417Z" fill="black" />
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M40.3968 70.9373C36.7897 67.0778 34.5833 61.8937 34.5833 56.1979C34.5833 50.3844 36.8831 45.107 40.6181 41.2198C38.9339 41.0919 37.2048 41.0227 35.4479 41.0227C23.9593 41.0227 13.7884 43.8966 7.47346 48.2022C2.656 51.4876 0 55.6618 0 60.0435V65.0616C0 66.6178 0.619042 68.1153 1.72225 69.2185C2.82546 70.3182 4.31946 70.9407 5.87917 70.9407L40.3968 70.9373Z" fill="black" />
@@ -153,16 +153,16 @@ const Home = () => {
 
                             <div className=' font-semibold'>Select Your Role and <br /> Sign Up</div>
                         </div>
-                        <div className=' bg-white h-48 w-72 text-center flex flex-col items-center p-8 '>
+                        <div className=' bg-white h-60 w-96  text-center flex flex-col items-center p-12 space-y-5'>
                             <svg width="64" height="64" viewBox="0 0 65 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M41.2534 13.4709H49.6764L41.2534 3.89185V13.4709Z" fill="black" />
                                 <path d="M50.1889 31.4928C50.9841 31.4928 51.7755 31.5426 52.5596 31.6415V18.2038H38.7921C37.4829 18.2038 36.5119 17.1958 36.5119 15.8889V0H0.951602C0.851121 0 0.76923 0.0815705 0.76923 0.182051V74.818C0.76923 74.9184 0.851121 75 0.951602 75H52.3774C52.478 75 52.5598 74.9184 52.5598 74.818V68.8439C51.7756 68.9429 50.9843 68.9926 50.1891 68.9926C40.6349 68.9926 32.7242 61.8348 31.5551 52.6091H8.79311V47.8761H31.555C32.724 38.6505 40.6349 31.4928 50.1889 31.4928ZM32.5 60.6189V65.3519H8.79311V60.6189H32.5ZM8.79311 9.64808H28.4881V14.3811H8.79311V9.64808ZM8.79311 22.3907H44.9005V27.1237H8.79311V22.3907ZM32.5 39.8665H8.79311V35.1335H32.5V39.8665Z" fill="black" />
                                 <path d="M50.1889 36.2258C42.4463 36.2258 36.1471 42.5138 36.1471 50.2428C36.1471 57.9718 42.4461 64.2598 50.1889 64.2598C57.9317 64.2598 64.2308 57.9718 64.2308 50.2428C64.2308 42.5138 57.9317 36.2258 50.1889 36.2258ZM49.9601 56.7401C49.159 57.5978 47.8024 57.7332 46.8473 57.0513L41.4117 53.172L44.1692 49.3218L47.9115 51.9927L55.7768 43.5709L59.2449 46.7984L49.9601 56.7401Z" fill="#EB7150" />
                             </svg>
 
-                            <div className=' font-semibold'>Buyers Post Your Requirements</div>
+                            <div className=' font-semibold'>Buyers Post Your <br /> Requirements</div>
                         </div>
-                        <div className=' bg-cyan-50 h-48 w-72 text-center flex flex-col items-center p-8 '>
+                        <div className=' bg-cyan-50 h-60 w-96 text-center flex flex-col items-center p-12 space-y-5'>
                             <svg width="64" height="64" viewBox="0 0 76 78" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M50.2778 73.2965H4.2403C1.81727 73.2965 0 71.3278 0 69.0562V4.2403C0 1.96871 1.96871 0 4.2403 0H50.2778C52.7008 0 54.5181 1.96871 54.5181 4.2403V33.1652C47.8548 28.622 38.7684 29.5306 32.8623 35.4368C26.3504 41.9486 26.5018 53.0037 33.1652 59.5156C38.9199 65.1188 47.8548 65.876 54.5181 61.4843V69.0562C54.5181 71.3278 52.7008 73.2965 50.2778 73.2965ZM29.0763 65.7246H8.32915C6.9662 65.7246 6.9662 63.6044 8.32915 63.6044H28.9249C30.4393 63.6044 30.4393 65.7246 29.0763 65.7246ZM24.836 56.3354H8.32915C6.9662 56.3354 6.9662 54.2152 8.32915 54.2152H24.836C26.3504 54.2152 26.3504 56.3354 24.836 56.3354ZM22.5644 47.0976H8.32915C6.9662 47.0976 6.9662 44.826 8.32915 44.826H22.5644C24.0788 44.826 24.0788 47.0976 22.5644 47.0976ZM25.5932 37.7083H8.32915C6.9662 37.7083 6.9662 35.4368 8.32915 35.4368H25.5932C27.1076 35.4368 27.1076 37.7083 25.5932 37.7083ZM46.0375 28.3191H8.32915C6.9662 28.3191 6.9662 26.0475 8.32915 26.0475H46.0375C47.5519 26.0475 47.5519 28.3191 46.0375 28.3191ZM35.7396 18.9299H18.7785C17.2641 18.9299 17.2641 16.6583 18.7785 16.6583H35.7396C37.254 16.6583 37.254 18.9299 35.7396 18.9299Z" fill="black" />
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M37.4053 39.6771C41.4942 35.4368 48.309 35.4368 52.5493 39.5257C59.364 46.3404 54.6694 58.0012 45.1287 58.0012C36.0424 58.0012 30.5906 46.9462 37.4053 39.6771ZM30.1363 47.4005C30.1363 34.0739 46.0374 27.562 55.4266 36.6483C61.0298 42.1001 61.4842 50.8836 56.4867 56.7897L60.4241 60.7271L58.9097 62.2415L54.9723 58.4556C45.7345 66.7847 30.2877 60.4243 30.1363 47.4005Z" fill="#EB7150" />
@@ -170,11 +170,11 @@ const Home = () => {
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M68.7533 77.5369C68.6018 77.5369 68.299 77.3854 67.9961 77.234L57.6982 67.0876C56.6381 66.0275 56.7896 64.3617 57.6982 63.453L61.6356 59.3642C62.5443 58.4555 64.3615 58.3041 65.4216 59.3642L75.7195 69.5106C76.0224 69.8135 76.1738 70.5707 75.7195 71.025L69.6619 77.0826C69.359 77.3854 69.0562 77.5369 68.7533 77.5369Z" fill="#EB7150" />
                             </svg>
 
-                            <div className=' font-semibold'>Review, Select, and Contact the Best Suppliers</div>
+                            <div className=' font-semibold'>Review, Select,and<br />  Contact the Best Suppliers</div>
                         </div>
                     </div>
                     <div className=" flex">
-                        <div className=' bg-white h-48 w-72 text-center flex flex-col items-center p-8 '>
+                        <div className=' bg-white h-60 w-96  text-center flex flex-col items-center p-12 space-y-5 '>
                             <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g clip-path="url(#clip0_1_215)">
                                     <mask id="mask0_1_215" maskUnits="userSpaceOnUse" x="0" y="0" width="75" height="75">
@@ -198,9 +198,9 @@ const Home = () => {
                                 </defs>
                             </svg>
 
-                            <div className=' font-semibold'>Suppliers Complete your profile and get notified for opportunities</div>
+                            <div className=' font-semibold'>Suppliers Complete your profile and get notified for <br /> opportunities</div>
                         </div>
-                        <div className=' bg-cyan-50 h-48 w-72 text-center flex flex-col items-center p-8 '>
+                        <div className=' bg-cyan-50 h-60 w-96  text-center flex flex-col items-center p-12 space-y-5 '>
                             <svg width="64" height="64" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g clip-path="url(#clip0_1_194)">
                                     <path d="M45.4258 69.4654L35.3054 69.4651V59.5411H15.1822V55.1522H39.4976L45.0324 49.6175H15.1822V45.2287H49.4212L54.956 39.6939H15.1822V35.3051H59.3448L65.0764 29.5734V2.76738C65.0764 1.24146 63.835 0 62.309 0C60.7831 0 59.5417 1.24146 59.5417 2.76738C59.5417 4.29302 60.7828 5.53418 62.3086 5.53418H62.309V9.923L9.92358 9.92344V75H65.0764V49.8149L45.4258 69.4654ZM15.1822 25.3816H39.9708V29.7705H15.1822V25.3816Z" fill="black" />
@@ -216,9 +216,9 @@ const Home = () => {
                                 </defs>
                             </svg>
 
-                            <div className=' font-semibold'>Contact to Buyers and Share your Quote for the service</div>
+                            <div className=' font-semibold'>Contact to Buyers and Share <br /> your Quote for the service</div>
                         </div>
-                        <div className=' bg-white h-48 w-72 text-center flex flex-col items-center p-8 '>
+                        <div className=' bg-white h-60 w-96  text-center flex flex-col items-center p-12 space-y-5'>
                             <svg width="64" height="64" viewBox="0 0 87 86" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M28.9361 70.393C30.1146 68.9165 29.8843 66.7492 28.4078 65.5707C27.7034 65.0018 26.9448 64.7308 26.1592 64.7308C26.0237 64.7308 25.8883 64.7444 25.7528 64.7579C24.8317 64.8663 24.0189 65.3269 23.4636 66.0583L18.9257 71.9237C18.4109 72.5739 18.1536 73.3054 18.1536 74.064C18.1536 75.107 18.6277 76.0823 19.4404 76.746C20.9034 77.9245 23.0707 77.6807 24.2628 76.2178L28.9361 70.393Z" fill="black" />
                                 <path d="M16.948 70.0272L21.5129 64.1483C22.0277 63.471 22.3121 62.6718 22.3121 61.8319C22.3121 60.667 21.7839 59.5833 20.8627 58.8518C20.2125 58.3371 19.4133 58.0662 18.587 58.0662C18.4245 58.0662 18.2619 58.0797 18.0994 58.0933C17.097 58.2152 16.2165 58.7299 15.6069 59.5156L11.042 65.3945C9.78219 67.0335 10.0667 69.3905 11.6922 70.6503C12.4914 71.2734 13.4667 71.5443 14.4555 71.4224C15.4579 71.314 16.3384 70.8128 16.948 70.0272Z" fill="black" />
@@ -231,36 +231,39 @@ const Home = () => {
                                 <path d="M41.4252 83.0718L42.0618 82.083C42.3869 81.5953 42.5495 81.0128 42.5495 80.4033C42.5495 79.3467 42.0212 78.3714 41.1542 77.816C40.1654 77.1794 38.8785 77.1794 37.8626 77.7889C37.7542 77.8431 37.7136 77.8973 37.6459 77.9515C37.6323 77.965 37.6052 77.9786 37.5917 77.9921C37.5781 77.9921 37.5781 78.0057 37.5646 78.0192C37.5104 78.0598 37.4427 78.1411 37.3885 78.1953C37.0092 78.6694 36.0474 79.9156 35.1941 81.0399C34.7064 81.7308 34.5168 82.5571 34.6658 83.3834C34.8283 84.2232 35.3024 84.9276 36.0204 85.3882L36.21 85.5101C36.8737 85.9435 37.7 86.1061 38.4857 85.93C39.2985 85.7539 39.9893 85.2798 40.4363 84.589L41.4252 83.0718C41.4116 83.0854 41.4116 83.0854 41.4252 83.0718Z" fill="black" />
                             </svg>
 
-                            <div className=' font-semibold'>Both the Parties can Connect and Make Business Leave a Feedback</div>
+                            <div className=' font-semibold'>Both the Parties can Connect <br /> and Make Business Leave a <br /> Feedback</div>
                         </div>
                     </div>
                 </div>
             </section>
 
 
-            <footer className=' bg-blue-950 h-64'>
-                <div className=' flex justify-around items-center p-10'>
-                    <div className=' flex space-x-10'>
-                        <div className='logo'>
-                            <img
-                                className=""
-                                src="./loho2.png"
-                                alt="Logo"
-                            />
-                            <h1 className=' text-white'> © R Singhania</h1>
+            <footer className="bg-blue-950 py-1 h-64">
+                {/* <!-- Top Border Line --> */}
+                <div className=" ml-52 mr-52 border-t border-gray-600 mt-10"></div>
+
+                {/* <!-- Main Footer Content --> */}
+                <div className="flex justify-around items-center p-10">
+                    <div className="flex space-x-10 items-center">
+                        {/* <!-- Logo Section --> */}
+                        <div className="logo  ">
+                            <img className="max-w-full mt-5 " src="./loho2.png" alt="Logo" />
+                            <h1 className="text-gray-300 text-xl mt-2">© R Singhania</h1>
                         </div>
-                        <div className="detels flex space-x-6">
-                            <div>
-                                <h1>Company</h1>
-                                <h3> About FAQ</h3>
+
+                        {/* <!-- Details Section --> */}
+                        <div className="detels flex space-x-6 text-white">
+                            <div className="space-y-2">
+                                <h1 className="font-semibold">Company</h1>
+                                <h3 className="font-thin text-gray-300">About <br /> FAQ</h3>
                             </div>
-                            <div>
-                                <h1>Company</h1>
-                                <h3> About FAQ</h3>
+                            <div className="space-y-2">
+                                <h1 className="font-semibold">Terms</h1>
+                                <h3 className="font-thin text-gray-300">Data privacy <br /> Terms <br /> Accessibility</h3>
                             </div>
-                            <div>
-                                <h1>Company</h1>
-                                <h3> About FAQ</h3>
+                            <div className="space-y-2">
+                                <h1 className="font-semibold">Related</h1>
+                                <h3 className="font-thin text-gray-300">Find Buyer <br /> Feedback</h3>
                             </div>
                         </div>
                     </div>
@@ -308,7 +311,9 @@ const Home = () => {
                         </svg>
 
                     </div>
+                    {/* </div> */}
                 </div>
+                <div className="ml-52 mr-52 border-t border-gray-600 mb-10"></div>
             </footer>
 
 
